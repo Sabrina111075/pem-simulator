7840ed5b2368392c27bd24072bad4396b657866f
Sabrina111075 <sabrina.hsueh@crystalmachine.com.tw>
2026-09-01 16:08:28 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -413,7 +413,7 @@ if stock_mode == "自訂股票代碼":
 # ==========================================
 st.markdown(
     f"""
-    <div style="margin-top: -15px; margin-bottom: 10px;">
+    <div style="margin-top: -45px; margin-bottom: 8px;">
         <h3 style="font-size: 24px; font-weight: 600; color: #1f2937; margin: 0;">
             📊 市場實時行情與 P/V/C 數據 ( 標的：{display_stock_name} )
         </h3>
@@ -423,7 +423,7 @@ st.markdown(
 )
 
 st.markdown(
-    f"##### 💡 PVCS 幾何流場實時診斷卡片 <span style='font-size: 14px; color: #64748b; font-weight: normal; margin-left: 10px;'>( 當前標的：:green[{display_stock_name}] )</span>", 
+    f"##### 💡 PVCS 幾何流場實時診斷卡片 <span style='font-size: 14px; color: #64748b; font-weight: normal; margin-left: 10px;'>( 當前標的：:green[{display_stock_name}] )</span>",
     unsafe_allow_html=True
 )
 
